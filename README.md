c57774374330852e495825de4bb08ab0f5f9d045
Zeqiang Lai <916323301@qq.com>
2023-07-09 17:29:29 +0800

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# dola kit
+
+A toolkit for personal use. Inspired by [kiuikit](https://github.com/ashawkey/kiuikit)
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dola',
+    author='Zeqiang Lai',
+    author_email='laizeqiang@outlook.com',
+    packages=find_packages(),
+    version='0.0.1',
+    include_package_data=True,
+)
